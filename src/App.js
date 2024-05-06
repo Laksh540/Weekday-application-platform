86d31e466061f5ab626357017e0d55dd3bb632fe
Laksh <laksh.prabhudessai@gmail.com>
2024-05-06 19:20:29 +0530

    converting response into text and updating  job list in useState

--- src/App.js
+++ src/App.js
@@ -78,6 +78,7 @@ const App = () => {
       inputValue: "",
     },
     companyName: "",
+    jobList: [],
   });
   const [toggleToRequestMoreData, setToggleToRequestMoreData] = useState(true);
 
@@ -86,14 +87,14 @@ const App = () => {
     const handleScroll = () => {
       const { scrollTop, clientHeight, scrollHeight } =
         document.documentElement;
-      console.log("scrollTop", scrollTop);
-      console.log("clientHeight", clientHeight);
-      console.log("scrollHeight", scrollHeight);
+      // console.log("scrollTop", scrollTop);
+      // console.log("clientHeight", clientHeight);
+      // console.log("scrollHeight", scrollHeight);
       if (scrollTop + clientHeight >= scrollHeight) {
         setToggleToRequestMoreData((prev) => !prev);
       }
     };
-
+    getJobList();
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
@@ -101,14 +102,8 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    // if(!isFirstRenderCheckToGetJobList){
-    // isFirstRenderCheckToGetJobList = false;
-    getJobList();
-    // }
-  }, []);
-
-  useEffect(() => {
     let getData;
+    // ignore for first render
     if (!isFirstRenderCheckToGetJobList) {
       getData = setTimeout(() => {
         getJobList();
@@ -132,7 +127,14 @@ const App = () => {
     });
 
     try {
-      const res = await JobService.getList(body);
+      const res = await JobService.getList(body).then((response) =>
+        response.text()
+      );
+      const parsedResponse = JSON.parse(res);
+      setPageObj((prevPageObj) => ({
+        ...prevPageObj,
+        jobList: updatedJobList(prevPageObj.jobList, parsedResponse?.jdList),
+      }));
       console.log("res", res);
     } catch (error) {
       console.log(error);
@@ -247,7 +249,30 @@ const App = () => {
       companyName: e?.target?.value,
     }));
   };
+  // helper
+  const updatedJobList = (oldData, newData) => {
+    let updatedList = [...oldData];
+    // updatedList
+    newData?.forEach((record) => {
+      updatedList.push({
+        companyName: record?.companyName ?? "",
+        jdLink: record?.jdLink ?? "",
+        jobDetailsFromCompany: record?.jobDetailsFromCompany ?? "",
+        jobRole: record?.jobRole ?? "",
+        location: record?.location ?? "",
+        logoUrl: record?.logoUrl ?? "",
+        maxExp: record?.maxExp ?? "",
+        maxJdSalary: record?.maxJdSalary ?? "",
+        minJdSalary: record?.minJdSalary ?? "",
+        minExp: record?.minExp ?? "",
+        salaryCurrencyCode: record?.salaryCurrencyCode ?? "",
+      });
+    });
 
+    return updatedList;
+  };
+
+  console.log("pageObj", pageObj);
   return (
     <div className="App ">
       <div className="filter-container one-rem-mb">
@@ -320,106 +345,119 @@ const App = () => {
       </div>
 
       <Grid container spacing={2} className="">
-        <Grid item xs={12} md={4}>
-          <Card className="card-layout shadow-2 border-radius-20">
-            <CardContent>
-              <div className="w-fit py-4 px-6 fs-9  b-1-gray shadow border-radius-10 one-rem-mb">
-                <span>⏳ Posted a month ago</span>
-              </div>
-              <p className="fs-13 fw-600 light-grey mt-0  half-rem-mb">
-                fampay{" "}
-              </p>
-              <p
-                // color="subtitle1"
-                // gutterBottom
-                className="fs-14 fw-400  mt-0 half-rem-mb"
-              >
-                Backend Engineer{" "}
-              </p>
-              <p className="fs-11 fw-500  mt-0 half-rem-mb">Bangalore </p>
-              <p
-                color=""
-                // variant="body1"
-                // gutterBottom
-                className="fs-14 dark-grey fw-400"
-              >
-                Estimated Salary: 18-35 LPA ✅
-              </p>
-              <div className="card-content one-rem-mb">
-                <p
-                  // variant="h6"
-                  component="div"
-                  className="fs-16 mb-0"
-                >
-                  About Company
-                </p>
-                <p
-                  color=""
-                  // variant="subtitle2"
-                  className="fs-14 fw-500 half-rem-mt half-rem-mb"
-                >
-                  About Us
-                </p>
-                <p className="content-fade m-0 fs-14">
-                  FamPay is building India's first neo-bank exclusively teens.
-                  FamPay helps teens make their own online and offline payments
-                  through UPI, Fam Pay App and FamCard. Our aim is to make
-                  banking cool for teens and to help them learn the value of
-                  money, savings and spending wisely. We are on a mission to
-                  raise a new, financially aware generation, and drive 250
-                  Million+ Indian teenagers to kickstart their financial journey
-                  super early in their life.
-                </p>
-                <Button
-                  size="small"
-                  variant="text"
-                  color="primary"
-                  className="text-capitalize"
-                  disableRipple
-                >
-                  View Job
-                </Button>
-              </div>
-              <div>
+        {pageObj?.jobList?.map((job, index) => (
+          <Grid item xs={12} md={4} key={index}>
+            <Card className="card-layout shadow-2 border-radius-20">
+              <CardContent>
+                <div className="w-fit py-4 px-6 fs-9  b-1-gray shadow border-radius-10 one-rem-mb">
+                  <span>⏳ Posted a month ago</span>
+                </div>
+                <div className="flex  item-start">
+                  <div className="w-38 h-38 half-rem-mr">
+                    <img
+                      src={`${job?.logoUrl ?? ""}`}
+                      className="w-38 h-38"
+                      alt=""
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="fs-13 fw-600 light-grey mt-0  half-rem-mb">
+                      fampay{" "}
+                    </p>
+                    <p
+                      // color="subtitle1"
+                      // gutterBottom
+                      className="fs-14 fw-400  mt-0 half-rem-mb"
+                    >
+                      Backend Engineer{" "}
+                    </p>
+                    <p className="fs-11 fw-500  mt-0 half-rem-mb">Bangalore </p>
+                  </div>
+                </div>
                 <p
                   color=""
                   // variant="body1"
                   // gutterBottom
-                  className="fs-14 dark-grey fw-400 mt-0 half-rem-mb"
+                  className="fs-14 dark-grey fw-400"
                 >
-                  Minimum Experience
+                  Estimated Salary: 18-35 LPA ✅
                 </p>
-                <p className="fs-14 fw-400 mt-0 half-rem-mb">2 Years</p>
-              </div>
-              <div>
-                <Button
-                  variant="contained"
-                  // color="success"
-                  fullWidth
-                  className="half-rem-mb half-rem-mt text-light-green text-1000 font-lexend text-capitalize"
-                  disableRipple
-                >
-                  ⚡ Easy Apply
-                </Button>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  className=" text-capitalize font-lexend fw-200"
-                  fullWidth
-                  startIcon={
-                    <div className="flex">
-                      <AccountCircleIcon className="half-rem-mr " />
-                      <AccountCircleIcon className=" " />
-                    </div>
-                  }
-                  disableRipple
-                >
-                  Unlock Referral Asks
-                </Button>
-              </div>
-            </CardContent>
-          </Card>
-        </Grid>
+                <div className="card-content one-rem-mb">
+                  <p
+                    // variant="h6"
+                    component="div"
+                    className="fs-16 mb-0"
+                  >
+                    About Company
+                  </p>
+                  <p
+                    color=""
+                    // variant="subtitle2"
+                    className="fs-14 fw-500 half-rem-mt half-rem-mb"
+                  >
+                    About Us
+                  </p>
+                  <p className="content-fade m-0 fs-14">
+                    FamPay is building India's first neo-bank exclusively teens.
+                    FamPay helps teens make their own online and offline
+                    payments through UPI, Fam Pay App and FamCard. Our aim is to
+                    make banking cool for teens and to help them learn the value
+                    of money, savings and spending wisely. We are on a mission
+                    to raise a new, financially aware generation, and drive 250
+                    Million+ Indian teenagers to kickstart their financial
+                    journey super early in their life.
+                  </p>
+                  <Button
+                    size="small"
+                    variant="text"
+                    color="primary"
+                    className="text-capitalize"
+                    disableRipple
+                  >
+                    View Job
+                  </Button>
+                </div>
+                <div>
+                  <p
+                    color=""
+                    // variant="body1"
+                    // gutterBottom
+                    className="fs-14 dark-grey fw-400 mt-0 half-rem-mb"
+                  >
+                    Minimum Experience
+                  </p>
+                  <p className="fs-14 fw-400 mt-0 half-rem-mb">2 Years</p>
+                </div>
+                <div>
+                  <Button
+                    variant="contained"
+                    // color="success"
+                    fullWidth
+                    className="half-rem-mb half-rem-mt text-light-green text-1000 font-lexend text-capitalize"
+                    disableRipple
+                  >
+                    ⚡ Easy Apply
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    className=" text-capitalize font-lexend fw-200"
+                    fullWidth
+                    startIcon={
+                      <div className="flex">
+                        <AccountCircleIcon className="half-rem-mr " />
+                        <AccountCircleIcon className=" " />
+                      </div>
+                    }
+                    disableRipple
+                  >
+                    Unlock Referral Asks
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
